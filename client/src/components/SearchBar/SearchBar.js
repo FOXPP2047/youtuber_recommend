70cfd752772279e2a6b9a0d46bf7370cf5151377
Yongmin Cho <qlxhdl55@gmail.com>
2020-06-05 17:44:34 -0700

    Link

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./SearchBar.css";
 
@@ -6,9 +7,11 @@ const SearchBar = () => {
     return (
         <div className="mainContainer">
             <input type="text" name="" className="textField" placeholder="Search Your Youtuber"/>
-            <a className="buttonField">
-                <i class="fas fa-search-plus"></i>
-            </a>
+            <Link to={"/youtuber"}>
+                <button className="buttonField">
+                    <i class="fas fa-search-plus"></i>
+                </button>
+            </Link>
         </div>
     );
 }
