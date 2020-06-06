d72b93038b3482d5905a299c06c6235a266db371
Yongmin Cho <qlxhdl55@gmail.com>
2020-06-06 00:11:00 -0700

    Link page

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./SearchBar.css";
 
 const SearchBar = () => {
+    const [typedValue, setTypedValue] = useState("");
+    function handleChange(event) {
+        setTypedValue(event.target.value);
+    }
+
     return (
         <div className="mainContainer">
-            <input type="text" name="" className="textField" placeholder="Search Your Youtuber"/>
-            <Link to={"/youtuber"}>
-                <button className="buttonField">
-                    <i class="fas fa-search-plus"></i>
-                </button>
+            <input type="text" name="textField" className="textField" autoComplete="off"
+             placeholder="Search Your Youtuber" onChange={event => handleChange(event)}/>
+            <Link to={"/" + typedValue}>
+                <div className="buttonField">
+                    <i className="fas fa-search-plus"></i>
+                </div>
             </Link>
         </div>
     );
